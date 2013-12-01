e2df06d20ed0856c8a26f64bb2f9ed87e87caafd
Oliver Manheim <omanheim@sas.upenn.edu>
2013-11-30 21:45:14 -0600

    text changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,4 @@
-var activeTab = 'none',
-    images = ['blur.jpg, noblur.jpg'],
-    image = 0,
-    font = 3,
-    fonts = ["'Ribeye', cursive", "'Quicksand', sans-serif", "'Trocchio', serif", "'Poiret One', cursive"],
-    inImages = false;
+var activeTab = 'none';
 
 $('#tabs span').click(function() {
     var tab = $(this).attr('id');
@@ -17,44 +12,5 @@ $('#tabs span').click(function() {
         $('#' + tab + 'Tab').show();
         if (!$('#slider').is(':visible')) $('#slider').slideToggle();
         activeTab = tab;
-    }
-});
-
-$('#images').click(function() {
-    other = image == 0 ? 'blur.jpg' : 'noblur.jpg';
-    if (false) {
-        if ($('#slider').is(':visible')) $('#slider').slideToggle();
-        $('html').css('background','black');
-        $('#pics').show();
-        inImages = true;
-    } else {
-        $('#pics').hide();
-        $('html').css('background', 'url(' + other + ') no-repeat center center fixed');
-        $('html').css('-webkit-background-size', 'cover');
-        $('html').css('-moz-background-size', 'cover');
-        $('html').css('-o-background-size', 'cover');
-        inImages = false;
-        other = (other + 1) % 1;
-    }
-});
-
-$(document).keyup(function(e) {
-    if (e.which == 70) {
-        font = (font + 1) % fonts.length;
-        $('html').css('font-family',fonts[font]);
-    }
-    if (e.which == 61) {
-        $('#head, #sub').each(function() {
-            var size = $(this).css('font-size').replace('px',''),
-                newSize = parseInt(size) + 3;
-            $(this).css('font-size', newSize + 'px');
-        });
-    }
-    if (e.which == 173) {
-        $('#head, #sub').each(function() {
-            var size = $(this).css('font-size').replace('px',''),
-                newSize = parseInt(size) - 3;
-            $(this).css('font-size', newSize + 'px');
-        });
     }
 });
