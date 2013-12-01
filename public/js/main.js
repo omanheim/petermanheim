cf86d00cb1f141e179adab34d356ddb5c9354d55
Oliver Manheim <omanheim@sas.upenn.edu>
2013-12-01 03:55:14 -0600

    small changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 var activeTab = 'none';
 
+sizeSlider();
+
 $('#tabs span').click(function() {
     var tab = $(this).attr('id');
     if (activeTab == tab) $('#slider').slideToggle();
@@ -7,10 +9,19 @@ $('#tabs span').click(function() {
         $('#slider .tab').each(function() {
             $(this).hide();
         });
-        if (tab == 'about') $('#slider').css('width', '1200px');
+        if (tab == 'about') $('#slider').css('width', '600px');
         else $('#slider').css('width', '500px');
         $('#' + tab + 'Tab').show();
         if (!$('#slider').is(':visible')) $('#slider').slideToggle();
         activeTab = tab;
     }
+});
+
+function sizeSlider() {
+    if ($(window).height() < 665) $('#slider').css('max-height', 100);
+    else $('#slider').css('max-height', 325);
+}
+
+$(window).on('resize', function() {
+    sizeSlider();
 });
